c4447a1ac34fa209baf63179ad0732ea9043f54f
Estebanoy <estebi101@gmail.com>
2022-08-29 10:20:52 -0300

    primer commit del repositorio

---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+let numero = parseInt(prompt("Ingrese numero para calcular tabla del 1 al 10"))
+
+for(let i=1; i<=10 ; i++){
+    let resultado = numero * i;
+    console.log(numero + "*" + i + "=" + resultado )
+}
+
+
