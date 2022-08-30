99f656868f6ddc3d27f3e92163191c1987c2e94a
Estebanoy <estebi101@gmail.com>
2022-08-30 18:15:27 -0300

    Se trabajo en el desafio 3

--- javascript/script.js
+++ javascript/script.js
@@ -4,7 +4,7 @@ for(let i=1; i<=10 ; i++){
     let resultado = numero * i;
     console.log(numero + "*" + i + "=" + resultado )
 }
-**/
+
 let cantidadCursos = parseInt(prompt("Ingrese la cantidad de cursos deseados"));
 
 function compras(){
@@ -41,4 +41,55 @@ function compras(){
     }
    
 }
-console.log(compras());
+console.log(compras()); **/
+
+
+function curso(nombre,tiempo,precio){
+        this.nombre = nombre;
+        this.tiempo = tiempo;
+        this.precio = precio;
+    }
+    
+
+    
+const cursos = [];  
+cursos.push(new curso("curso1",3,150));
+cursos.push(new curso("curso2",4,200))
+cursos.push(new curso("curso3",3,250))
+cursos.push(new curso("curso4",2,100))
+cursos.push(new curso("curso5",5,350))
+
+
+const newCursoName = prompt("Ingrese nombre de nuevo curso");
+const newCursotime = parseInt(prompt("Ingrese la duracion del nuevo curso(un numero , son semanas)"));
+const newCursoPrice = parseInt(prompt("Ingrese el monto deseado para el nuevo curso"));
+
+const curso6 = new curso(newCursoName,newCursotime,newCursoPrice);
+
+cursos.push(curso6);
+
+document.write("Nombre del nuevo curso = " + curso6.nombre + "<br>" + "Duracion del curso(semanas) = " + curso6.tiempo + "<br>" + "Precio del curso =$ " + curso6.precio);
+
+const borrarCurso = prompt("Ingrese el nombre del curso que desea borrar(por ejemplo: 'curso1,curso2,curso3')");
+
+for(const ref of cursos){
+    if(ref.nombre === borrarCurso){
+        let indice = cursos.indexOf(ref);
+        cursos.splice(indice,1)
+        console.log("Se a elminado el " + ref.nombre);
+    }
+}
+
+for(const ref of cursos){
+    console.log(ref.nombre);
+    console.log(ref.tiempo+" Semanas");
+    console.log(ref.precio+" $");
+    console.log(cursos.indexOf(ref));
+}
+
+console.log(cursos);
+
+
+
+
+
